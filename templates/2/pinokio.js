d96b4c42cc783d3bbd4fdc70b57eb251777a3c51
cocktailpeanut <cocktailpeanuts@proton.me>
2025-04-12 14:27:52 -0700

    update

--- templates/2/pinokio.js
+++ templates/2/pinokio.js
@@ -10,7 +10,8 @@ module.exports = {
       install: info.running("install.js"),
       start: info.running("start.js"),
       update: info.running("update.js"),
-      reset: info.running("reset.js")
+      reset: info.running("reset.js"),
+      link: info.running("link.js")
     }
     if (running.install) {
       return [{
@@ -55,6 +56,13 @@ module.exports = {
           text: "Resetting",
           href: "reset.js",
         }]
+      } else if (running.link) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Deduplicating",
+          href: "link.js",
+        }]
       } else {
         return [{
           default: true,
@@ -72,11 +80,11 @@ module.exports = {
         }, {
           icon: "fa-solid fa-file-zipper",
           text: "<div><strong>Save Disk Space</strong><div>Deduplicates redundant library files</div></div>",
-          href: "link.json",
+          href: "link.js",
         }, {
           icon: "fa-regular fa-circle-xmark",
           text: "<div><strong>Reset</strong><div>Revert to pre-install state</div></div>",
-          href: "reset.json",
+          href: "reset.js",
           confirm: "Are you sure you wish to reset the app?"
 
         }]
